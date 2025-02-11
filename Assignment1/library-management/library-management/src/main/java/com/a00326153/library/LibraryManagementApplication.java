3553251e56146b0f2e9a9bc71d334055c4208d64
PhilipHerweling <miggymorette@gmail.com>
2025-02-11 11:13:29 +0000

    Spring initializr folder
    Added the brief and spring initializr folder which i will use to build my microservice

---
+++ Assignment1/library-management/library-management/src/main/java/com/a00326153/library/LibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.a00326153.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementApplication.class, args);
+	}
+
+}
---
+++ Assignment1/library-management/library-management/src/test/java/com/a00326153/library/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a00326153.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
